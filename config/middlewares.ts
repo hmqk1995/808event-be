c31f810c41e11bd1cae378de107ad5bbc13e0b68
Kun Qian <qiankun1995@gmail.com>
2023-02-03 19:09:43 -1000

    feat: adding more domains

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -6,7 +6,14 @@ export default [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:1337', 'http://127.0.0.1:1337'],
+      origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:1337',
+        'http://127.0.0.1:1337',
+        'http://192.168.8.239:3000',
+        'http://kun.dev:3000',
+      ],
     }
   },
   'strapi::poweredBy',
