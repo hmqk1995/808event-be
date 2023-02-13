3b5c694add1bd146cb687e18642fc17679fec47b
Kun Qian <qiankun1995@gmail.com>
2023-02-12 23:44:13 -1000

    feat: w3 provider working

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,6 +19,7 @@ module.exports = ({ env }) => ({
         accessKeyId: env('DO_SPACE_ACCESS_KEY'),
         secretAccessKey: env('DO_SPACE_SECRET_KEY'),
         endpoint: env('DO_SPACE_ENDPOINT'),
+        // region: env('DO_SPACE_REGION'),
         params: {
           Bucket: env('DO_SPACE_BUCKET'),
         },
