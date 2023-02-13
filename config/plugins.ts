6949637f13c488d3280fee4f4b1f2e1f89de1356
Kun Qian <qiankun1995@gmail.com>
2023-02-12 19:53:19 -1000

    feat: try using aws provider to save uploads/ content on storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-export default {
+module.exports = ({ env }) => ({
   graphql: {
     config: {
       endpoint: '/graphql',
@@ -12,4 +12,22 @@ export default {
     },
   },
   ckeditor: true,
-};
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('S3_ACCESS_KEY_ID', 'DO003QBBUGVHADFRAHAG'),
+        secretAccessKey: env('S3_ACCESS_SECRET', 'd/9tIN67jgm5C9BoABDFm9+c11zZ7YvciEvDTPepJvo'),
+        region: env('S3_REGION', 'us-east-1'),
+        params: {
+          Bucket: env('S3_BUCKET', '808event-uploads'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
